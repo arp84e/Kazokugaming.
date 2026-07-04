086795f18c8604319decfe432344e96519c62098
arp84e <ajrojasp84e+1@gmail.com>
2026-07-04 19:49:09 +0200

    Delete src directory

--- src/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr';
-
-// Este cliente se utilizará principalmente en componentes con 'use client'
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
